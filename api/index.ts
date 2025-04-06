f2105b7e550b51b503b65ea4c3e4fbb116bed1b3
MayankTaneja-fnn <148222428+MayankTaneja-fnn@users.noreply.github.com>
2025-04-06 14:22:34 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,14 @@
+// api/index.ts
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import express from 'express';
 
 const app = express();
 
-app.get('/api/hello', (_req, res) => {
-  res.json({ message: 'Hello from Express on Vercel!' });
+app.get('/api/hello', (req, res) => {
+  res.json({ message: "Hello from Vercel Express!" });
 });
 
-// Let Vercel handle the request
+// Export handler
 export default (req: VercelRequest, res: VercelResponse) => {
   return app(req, res);
 };
